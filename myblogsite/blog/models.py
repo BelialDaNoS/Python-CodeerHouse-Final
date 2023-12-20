2c301840fb8952fa751a70c1cf046bd40ff60558
Belial <lautarogazzo1@gmail.com>
2023-12-19 22:53:18 -0300

    Vaque riendo

--- myblogsite/blog/models.py
+++ myblogsite/blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200)
     body = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='blog_images/')
