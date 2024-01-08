d5031f06ef513ef733667ca4675f57c3dcfd8c70
Belial <lautarogazzo1@gmail.com>
2024-01-08 20:06:08 -0300

    Cambio de Avatar por Foto de perfil. Intento 1

--- myblogsite/accounts/models.py
+++ myblogsite/accounts/models.py
@@ -5,4 +5,4 @@ class CustomUser(AbstractUser):
     profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     website = models.URLField(blank=True, null=True)
-    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+
