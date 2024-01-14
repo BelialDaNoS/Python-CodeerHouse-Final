1b445815e1c8a0bfc55ebc8f20e043faa0dbbe0e
Belial <lautarogazzo1@gmail.com>
2024-01-13 21:10:09 -0300

    Antes del desastre

--- myblogsite/myblogsite/urls.py
+++ myblogsite/myblogsite/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+import debug_toolbar
+
 
 urlpatterns = [
     path('', views.landing, name='landing'),
@@ -34,3 +36,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
