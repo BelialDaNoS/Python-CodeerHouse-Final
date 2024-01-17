79ca823ac20b22344fa96fae33b4f12f60cbb050
Belial <lautarogazzo1@gmail.com>
2024-01-17 19:39:42 -0300

    Empezamos

--- miBlog/static/js/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log("El DOM está completamente cargado y parseado");
-    // Aquí puedes agregar más código JavaScript según sea necesario
-});
